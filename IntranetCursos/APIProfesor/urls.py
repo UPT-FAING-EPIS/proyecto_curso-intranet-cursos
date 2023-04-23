e6408642c1ac8c94f5490bcc5913c83dffe591d5
Erick Mauricio <coartac.erick.mamani@gmail.com>
2023-04-23 16:11:09 -0500

    Se actualizo api profesores

--- IntranetCursos/APIProfesor/urls.py
+++ IntranetCursos/APIProfesor/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import APIProfesorViews
 urlpatterns=[
     path('',APIProfesorViews.as_view(),name='profesores_lists'),
-    #guia = path('<int:CodDocente>',APICreditosViews.as_view(), name='profesores_process')
+    path('<int:CodigoDocente>',APIProfesorViews.as_view(), name='Pofesores_perId')
 ]
