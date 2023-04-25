addaebf161ada5bab448380cf2550e774d4ba774
HelbertCLinUPT <hc2020067571@virtual.upt.pe>
2023-04-25 15:12:55 -0500

    nuevoCretos

---
+++ IntranetCursos/APICreditos/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import CreditoViews
+
+urlpatterns=[
+    path('creditos/',CreditoViews.as_view(), name='creditos_lists'),
+    path('creditos/<int:CodCredito>',CreditoViews.as_view(), name='creditos_process')
+]
