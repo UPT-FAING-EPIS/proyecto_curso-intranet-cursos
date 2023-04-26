e2c13a97cf62a3855dc5dfcf3083120aaed8a05c
CARLOS EDUARDO AGUILAR SOTO <caraguilarso@upt.pe>
2023-04-25 20:11:16 -0500

    Mejora de profesor

--- IntranetCursos/APIProfesor/models.py
+++ IntranetCursos/APIProfesor/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from APICursos.urls import TbEstado
 
 # Create your models here.
+class TbProfesor(models.Model):
+    CodigoProfesor = models.AutoField(primary_key=True)
+    NombreProfesor= models.CharField(max_length=50)
+    ApellidoProfesor= models.CharField(max_length=50)
+    EmailProfesor= models.CharField(max_length=50)
+    NumeroProfesor= models.PositiveIntegerField()
+    DireccionProfesor= models.CharField(max_length=50)
+    FkEstado_id = models.ForeignKey(TbEstado, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'TbProfesor'
